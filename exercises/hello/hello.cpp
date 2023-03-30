cf4566998b4ba5f77c1fd69771fc55abfb1e87d8
Daniel Kamen <102763696+danielkamen@users.noreply.github.com>
2023-03-29 21:32:29 -0400

    Delete exercises directory

--- exercises/hello/hello.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(void)
-{
- cout<<"|) /\ |\| | [- |_   /< /\ |\/| [- |\| - Daniel Kamen:"<<endl;
- //cout<<"Hello world! "+to_string(123)<<endl;
-}
